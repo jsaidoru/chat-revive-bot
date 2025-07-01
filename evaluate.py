de07d0582dd7d507aa5aaf66f7209ab0ff699d51
John Ridge <johnridgevn@gmail.com>
2025-07-01 14:58:34 +0700

    added stockfish

--- evaluate.py
+++ evaluate.py
@@ -1,9 +1,11 @@
 import chess
 import chess.engine
+import os
+print(os.path.exists(r"C:\Users\DEFAULTUSER\Downloads\stockfish-windows-x86-64-avx2\stockfish\stockfish-windows-x86-64-avx2.exe"))
 
 def evaluate(fen: str, time_limit=0.5) -> str:
     board = chess.Board(fen)
-    engine = chess.engine.SimpleEngine.popen_uci(r"C:\Users\DEFAULTUSER\Downloads\stockfish-windows-x86-64-avx2\stockfish\stockfish-windows-x86-64-avx2.exe")
+    engine = chess.engine.SimpleEngine.popen_uci("./stockfish/stockfish.exe")
 
     try:
         info = engine.analyse(board, chess.engine.Limit(time=time_limit))
