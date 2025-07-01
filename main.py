73db4b79714810f4d83e2e0b990e280b95931028
John Ridge <johnridgevn@gmail.com>
2025-07-01 08:43:37 +0700

    update

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import discord
 from discord.ext import commands
 import random
 import os
+from dotenv import load_dotenv
+load_dotenv()
 
 intents = discord.Intents.default()
 intents.message_content = True
