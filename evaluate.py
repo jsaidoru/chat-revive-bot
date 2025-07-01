bf5e75325b68fcf4d53455e7aa4e364d1ea7ff4f
John Ridge <johnridgevn@gmail.com>
2025-07-01 14:49:55 +0700

    bugfix

--- evaluate.py
+++ evaluate.py
@@ -3,7 +3,7 @@ import chess.engine
 
 def evaluate(fen: str, time_limit=0.5) -> str:
     board = chess.Board(fen)
-    engine = chess.engine.SimpleEngine.popen_uci("C:\Users\DEFAULTUSER\Downloads\stockfish-windows-x86-64-avx2\stockfish\stockfish-windows-x86-64-avx2.exe")
+    engine = chess.engine.SimpleEngine.popen_uci(r"C:\Users\DEFAULTUSER\Downloads\stockfish-windows-x86-64-avx2\stockfish\stockfish-windows-x86-64-avx2.exe")
 
     try:
         info = engine.analyse(board, chess.engine.Limit(time=time_limit))
