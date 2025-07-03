b1ce047ee099c811c027f675b70c4aee0ab504b8
Emily <emily@artyom.me>
2025-07-03 11:59:59 +0200

    move execute to cogs/

--- main.py
+++ main.py
@@ -1,13 +1,9 @@
 import discord
 from discord.ext import commands
-from discord.utils import escape_markdown, escape_mentions
 import random as rand
 import os
 from dotenv import load_dotenv
 import asyncio
-import asyncio
-import textwrap
-from is_safe_code import is_safe_ast
 
 load_dotenv()
 
@@ -68,53 +64,6 @@ async def load():
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
             await bot.load_extension(f"cogs.{filename[:-3]}")
-
-
-safe_builtins = {"print": print, "range": range, "len": len}
-
-safe_globals = {"__builtins__": safe_builtins}
-
-
-@bot.command("Execute Python codes")
-@commands.cooldown(rate=1, per=30, type=commands.BucketType.user)
-async def execute(ctx, *, code: str):
-    await ctx.send("This command might be removed from the future")
-    is_safe, reason = is_safe_ast(code)
-    if not is_safe:
-        return await ctx.send(f"❌ Unsafe code blocked: {reason}")
-    code = textwrap.dedent(code)
-
-    try:
-        proc = await asyncio.create_subprocess_exec(
-            "python",
-            "sandbox.py",
-            stdin=asyncio.subprocess.PIPE,
-            stdout=asyncio.subprocess.PIPE,
-            stderr=asyncio.subprocess.PIPE,
-        )
-
-        stdout, stderr = await asyncio.wait_for(
-            proc.communicate(code.encode()),
-            timeout=3,  # ⏱️ 3-second timeout
-        )
-
-        output = stdout.decode().strip()
-        if not output:
-            output = "✅ Code executed with no output."
-
-    except asyncio.TimeoutError:
-        output = "❌ Timeout: Your code ran too long."
-    except Exception as e:
-        output = f"❌ Execution error: {e}"
-
-    clean_output = escape_markdown(escape_mentions(output[:500]))
-    if any(x in clean_output for x in ["@everyone", "@here", "<@&", "<@"]):
-        await ctx.send("❌ What do you think you are trying to do?.")
-        return
-    await ctx.send(
-        f"First 500 characters of the result: \n {clean_output}",
-        allowed_mentions=discord.AllowedMentions.none(),
-    )
 
 
 TOKEN = os.environ.get("BOT_TOKEN")
