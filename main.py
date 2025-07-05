774236007f8c5c2fbb7f073d1e39e9cf6349e867
John Ridge <johnridgevn@gmail.com>
2025-07-05 11:20:57 +0700

    moved commands to a new folder

--- main.py
+++ main.py
@@ -1,16 +1,13 @@
 import discord
 from discord.ext import commands
-from discord.utils import escape_markdown, escape_mentions
-import random as rand
 import os
 from dotenv import load_dotenv
 import asyncio
-from tinydb import TinyDB, Query
-import time
-import datetime
+import other_cmd.roll
+import other_cmd.youcanonlyusethisonceinyourlife
+import other_cmd.help
 
 load_dotenv()
-
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -19,7 +16,6 @@ intents.guilds = True
 intents.members = True
 bot = commands.Bot(command_prefix=">", intents=intents)
 BOT_OWNER_ID = 1085862271399493732
-pending_suggestions = {}  # Stores user who suggested
 
 
 # === BOT EVENTS ===
@@ -38,9 +34,6 @@ async def on_message(message):
 Type `>help` to see my commands.
 """
         await message.channel.send(response)
-    if message.content.count("|") >= 50:
-        await message.channel.send("dont abuse discord bugs")
-        return
     await bot.process_commands(message)  # IMPORTANT!1!!11!
 
 
@@ -51,78 +44,24 @@ async def on_command_error(ctx, error):
     else:
         await ctx.send(f"⚠️ An error occurred: `{str(error)}`")
 
-
-# Remove default help
-
-bot.remove_command("help")
-
-
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
-
-
-
-@bot.command(
-    help="Randomly pick an option from the choices, separate each choices with a comma"
-)
-@commands.cooldown(rate=1, per=1, type=commands.BucketType.user)
-async def roll(ctx, *, choices: str):
-    if "ping " in choices.lower():
-        await ctx.send(
-            "Please don't randomly pick who to ping. I don't want anyone to blame my bot."
-        )
-    clean_choices = escape_mentions(escape_markdown(choices))
-    items = [item.strip() for item in clean_choices.split(",")]
-    if not items:
-        await ctx.send("No valid options provided.")
-        return
-    choice = rand.choice(items)
-    await ctx.send(f"You rolled: **{choice}**")
-    
-# Driver program
-# Use your persistent /storage folder
-cooldown_db = TinyDB('/storage/cooldowns.json')
-User = Query()
-
-def hhmmss(second):
-    return str(datetime.timedelta(seconds = second))
-
-@bot.command()
-async def youcanonlyusethisonceinyourlife(ctx):
-    user_id = str(ctx.author.id)
-    now = int(time.time())
-    lifetime = 2147483647  # about 68 years
-
-    # Check if user is already in cooldown
-    record = cooldown_db.get(User.user_id == user_id)
-    if record and now < record["cooldown_until"]:
-        remaining = record["cooldown_until"] - now
-        year = remaining / 31556926
-        await ctx.send(f"⏳ Wait {hhmmss(remaining)} (or {year:.9f} years) :3")
-        return
-    await ctx.send(f"<@{ctx.author.id}>\n🎉 Congrats, you can use this command again. Thank you for using Chat Revival Bot!")
-    # Set new cooldown
-    cooldown_db.upsert(
-        {"user_id": user_id, "cooldown_until": now + lifetime},
-        User.user_id == user_id
-    )
-
-    await ctx.send("✅ You used this command! See you in 68 years.")
 
 async def load():
     for filename in os.listdir("./cogs"):
         if filename.endswith(".py"):
             await bot.load_extension(f"cogs.{filename[:-3]}")
+bot.remove_command("help")
 
+bot.add_command(other_cmd.roll)
+bot.add_command(other_cmd.youcanonlyusethisonceinyourlife)
+bot.add_command(other_cmd.help)
 
 TOKEN = os.environ.get("BOT_TOKEN")
-
-
 async def main():
     async with bot:
         await load()
         await bot.start(TOKEN)
-
 
 asyncio.run(main())
