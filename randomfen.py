9babd530129931f1b98063caf6a58e47948a6200
John Ridge <johnridgevn@gmail.com>
2025-07-08 14:31:37 +0700

    increase cooldown for random fen

--- randomfen.py
+++ randomfen.py
@@ -2,9 +2,9 @@ import chess
 import random
 
 
-def random_fen(max_plies=100):
+def random_fen(min_plies=10, max_plies=476):
     board = chess.Board()
-    num_plies = random.randint(0, max_plies)
+    num_plies = random.randint(min_plies, max_plies)
 
     for _ in range(num_plies):
         if board.is_game_over():
