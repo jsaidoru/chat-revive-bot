65198831788d837e1f70bb456c27d3fdc49b32c8
John Ridge <johnridgevn@gmail.com>
2025-07-21 17:09:17 +0700

    added pi command to main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from discord.ext import commands
 import os
 from dotenv import load_dotenv
 import asyncio
-from other_cmd import roll, help, youcanonlyusethisonceinyourlife, pingeveryone, ask, colonthree, coolify
+from other_cmd import roll, help, youcanonlyusethisonceinyourlife, pingeveryone, ask, colonthree, coolify, pi
 # , info
 
 load_dotenv(dotenv_path=".env")
@@ -62,6 +62,7 @@ bot.add_command(ask.ask)
 # bot.add_command(info.info)
 bot.add_command(colonthree.colonthree)
 bot.add_command(coolify.coolify)
+bot.add_command(pi.pi)
 
 TOKEN = os.environ.get("BOT_TOKEN")
 async def main():
