f9f2dc8150bad8e3aa5fa42533666a63d160794e
John Ridge <johnridgevn@gmail.com>
2025-07-21 17:21:59 +0700

    debug

--- other_cmd/pi.py
+++ other_cmd/pi.py
@@ -36,6 +36,6 @@ def get_nth_digit(filename, n):
 @commands.command()
 async def pi(ctx):
     n = get_current_position()
-    digit = get_nth_digit("app/digitsofpi.txt", n)
+    digit = get_nth_digit("app/other_cmd/digitsofpi.txt", n)
     await ctx.send(f"Digit #{n} of π: `{digit}`")
     increment_position()
