43e040293b3df286f9ea7c1f59b41a2edbd30e60
John Ridge <129057739+jsaidoru@users.noreply.github.com>
2025-09-07 21:05:28 +0700

    main.py を更新

--- main.py
+++ main.py
@@ -76,9 +76,10 @@ async def on_reaction_add(reaction, user):
     if isinstance(reaction.emoji, discord.Emoji) or isinstance(reaction.emoji, discord.PartialEmoji):
         if reaction.emoji.id == 1363718257835769916: # KEKW
             if not kekwdb.contains(User.id == receiver_id):
+                   receiver_id = reaction.message.author.id
                 kekwdb.insert({'id': user.id, 'count': 1})
             else:
-                receiver_id = reaction.message.author.id
+                
                 user_data = kekwdb.get(User.id == receiver_id)
                 if user_data is not None:
                     kekwdb.update({'count': user_data['count'] + 1}, User.id == user.id) # type: ignore
