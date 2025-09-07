d8809caf5ce597d882999fd5a3dce098030fdad7
John Ridge <129057739+jsaidoru@users.noreply.github.com>
2025-09-07 18:59:06 +0700

    balance.py を更新

--- other_cmd/balance.py
+++ other_cmd/balance.py
@@ -39,8 +39,8 @@ async def balance(ctx, *, name: str | None = None):
         skull_count = skull_entry["count"]  # type: ignore
         description += f"**{skull_count}** <:iosskull:1413708504060924004>\n"
     
-    ratio = kekw_count/skull_count*100
-    description += f"**<:KEKW:1363718257835769916>-<:iosskull:1413708504060924004>** Ratio: {kekw_count/skull_count}%\n"
+    ratio = (kekw_count/skull_count)*100
+    description += f"**<:KEKW:1363718257835769916>-<:iosskull:1413708504060924004> Ratio**: {ratio}%\n"
     await ctx.send(embed=discord.Embed(
         title=f"{member}'s Balance",
         description=description,
